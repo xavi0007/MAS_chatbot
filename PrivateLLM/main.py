920803345445c3dd0748481e20e4726b404732d6
Xavier <XAVIER002@e.ntu.edu.sg>
2024-08-21 14:00:35 +0800

    Update main.py

--- PrivateLLM/main.py
+++ PrivateLLM/main.py
@@ -120,4 +120,4 @@ def create_app(test_config=None):
 #         return render_template('login.html')
 
 if __name__ =='__main__':
-    app.run(debug=True, host='172.21.47.117', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
