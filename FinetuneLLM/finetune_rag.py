438d9a45ead6ff3f3598aa0144836b86c517b1bf
xavi0007 <XAVIER002@e.ntu.edu.sg>
2024-11-05 15:22:57 +0800

    reranker added

--- FinetuneLLM/finetune_rag.py
+++ FinetuneLLM/finetune_rag.py
@@ -6,9 +6,11 @@ from llama_index.core.schema import MetadataMode,TextNode
 from llama_index.finetuning import generate_qa_embedding_pairs,SentenceTransformersFinetuneEngine
 from llama_index.core.evaluation import EmbeddingQAFinetuneDataset
 # from llama_index.llms.gemini import Gemini
-from llama_index.core import VectorStoreIndex
+from llama_index.core import VectorStoreIndex, QueryBundle
 from llama_index.core.postprocessor import LLMRerank
-
+from llama_index.core.retrievers import VectorIndexRetriever
+from copy import deepcopy
+from IPython.display import display, HTML
 from tqdm.notebook import tqdm
 import pandas as pd
 import os
@@ -20,12 +22,16 @@ class RAG_LLM:
         Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
         # ollama
         Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
+        Settings.chunk_overlap = 0
+        Settings.chunk_size = 128
+
         self.GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
         self.TRAIN_FILES = ["~/Private_LLM/data/10k/lyft_2021.pdf"]
         self.VAL_FILES = ["~/Private_LLM/data/10k/uber_2021.pdf"]
         self.INPUT_FILE = ["~/Private_LLM/data/paul_graham_essay.txt"]
-
+    
         self.TRAIN_CORPUS_FPATH = "~/Private_LLM/data/10k/train_corpus.json"
         self.VAL_CORPUS_FPATH = "~/Private_LLM/data/10k/val_corpus.json"
         
@@ -63,6 +69,45 @@ class RAG_LLM:
         query_engine = self.RAG_index.as_query_engine()
         response = query_engine.query(query)
         print(response)
+
+    def get_retrieved_nodes(self, query_str, vector_top_k=10, reranker_top_n=3, with_reranker=False
+        ):
+        query_bundle = QueryBundle(query_str)
+        # configure retriever
+        retriever = VectorIndexRetriever(
+            index=self.RAG_index,
+            similarity_top_k=vector_top_k,
+        )
+        retrieved_nodes = retriever.retrieve(query_bundle)
+
+        if with_reranker:
+            # configure reranker
+            reranker = LLMRerank(
+                choice_batch_size=5,
+                top_n=reranker_top_n,
+            )
+            retrieved_nodes = reranker.postprocess_nodes(
+                retrieved_nodes, query_bundle
+            )
+
+        return retrieved_nodes
+
+
+    def pretty_print(self, df):
+        return display(HTML(df.to_html().replace("\\n", "<br>")))
+
+    def visualize_retrieved_nodes(self, nodes) -> None:
+        result_dicts = []
+        for node in nodes:
+            node = deepcopy(node)
+            node.node.metadata = None
+            node_text = node.node.get_text()
+            node_text = node_text.replace("\n", " ")
+
+            result_dict = {"Score": node.score, "Text": node_text}
+            result_dicts.append(result_dict)
+
+        self.pretty_print(pd.DataFrame(result_dicts))
 
 
     def generate_dataset(self):
@@ -132,17 +177,35 @@ class RAG_LLM:
         return eval_results, hit_rate
 
 
+#Unit tests functions, cannot write another py script due to file referencing errors
 
 rag = RAG_LLM()
 text_prompt="summarise this file"
 rag.ask_RAG(text_prompt)
+
 #Run to get corpus data
 # train, val = rag.generate_dataset()
+
 val_dataset = EmbeddingQAFinetuneDataset.from_json("/home/xavier002/Private_LLM/data/10k/val_dataset.json")
 train_dataset = EmbeddingQAFinetuneDataset.from_json("/home/xavier002/Private_LLM/data/10k/train_dataset.json")
 
-#before
-_, hitrate_before = rag.evaluate_RAG(val_dataset)
-#after
-ft_emodel = rag.train_RAG(train_dataset, val_dataset)
-_, hitrate_after = rag.evaluate_RAG(val_dataset, ft_emodel)
+#before, and after
+# _, hitrate_before = rag.evaluate_RAG(val_dataset)
+# ft_emodel = rag.train_RAG(train_dataset, val_dataset)
+# _, hitrate_after = rag.evaluate_RAG(val_dataset, ft_emodel)
+
+#reranking
+new_nodes_before = rag.get_retrieved_nodes(
+    "What is Lyft's response to COVID-19?", vector_top_k=5, with_reranker=False
+)
+print(new_nodes_before)
+# rag.visualize_retrieved_nodes(new_nodes_before)
+
+new_nodes_after = rag.get_retrieved_nodes(
+    "What is Lyft's response to COVID-19?",
+    vector_top_k=20,
+    reranker_top_n=5,
+    with_reranker=True,
+)
+print(new_nodes_after)
+# rag.visualize_retrieved_nodes(new_nodes_after)
