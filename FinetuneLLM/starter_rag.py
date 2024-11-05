933ad7dd57439dd7dd869cc45a696374419586fb
xavi0007 <XAVIER002@e.ntu.edu.sg>
2024-11-05 14:11:52 +0800

    bug fixes

--- FinetuneLLM/starter_rag.py
+++ FinetuneLLM/starter_rag.py
@@ -5,8 +5,10 @@ from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.schema import MetadataMode,TextNode
 from llama_index.finetuning import generate_qa_embedding_pairs,SentenceTransformersFinetuneEngine
 from llama_index.core.evaluation import EmbeddingQAFinetuneDataset
-from llama_index.llms.gemini import Gemini
+# from llama_index.llms.gemini import Gemini
 from llama_index.core import VectorStoreIndex
+from llama_index.core.postprocessor import LLMRerank
+
 from tqdm.notebook import tqdm
 import pandas as pd
 import os
@@ -24,8 +26,8 @@ class RAG_LLM:
         self.VAL_FILES = ["~/Private_LLM/data/10k/uber_2021.pdf"]
         self.INPUT_FILE = ["~/Private_LLM/data/paul_graham_essay.txt"]
 
-        self.TRAIN_CORPUS_FPATH = "~/Private_LLM/data/train_corpus.json"
-        self.VAL_CORPUS_FPATH = "~/Private_LLM/data/val_corpus.json"
+        self.TRAIN_CORPUS_FPATH = "~/Private_LLM/data/10k/train_corpus.json"
+        self.VAL_CORPUS_FPATH = "~/Private_LLM/data/10k/val_corpus.json"
         
         #preload
         self.RAG_index = self.read_file(self.TRAIN_FILES)
@@ -62,21 +64,28 @@ class RAG_LLM:
         response = query_engine.query(query)
         print(response)
 
-    def train_RAG(self) -> object:
+
+    def generate_dataset(self):
         train_nodes = self.load_corpus(self.TRAIN_FILES, verbose=True)
         val_nodes = self.load_corpus(self.VAL_FILES, verbose=True)
 
-        train_dataset = generate_qa_embedding_pairs(
-                        llm=Ollama(model="llama3", request_timeout=360.0),
-                        nodes=train_nodes,
-                        output_path="train_dataset.json",
-                    )
+        # train_dataset = generate_qa_embedding_pairs(
+        #                 llm=Ollama(model="llama3", request_timeout=360.0),
+        #                 nodes=train_nodes,
+        #                 output_path="train_dataset.json",
+        #             )
+        train_dataset = None
         val_dataset = generate_qa_embedding_pairs(
-                        llm=Gemini(model_name="gemini-1.5-flash", api_key=self.GOOGLE_API_KEY),
+                        # llm=Gemini(model_name="models/gemini-1.5-flash", api_key=self.GOOGLE_API_KEY),
+                        llm = Ollama(model="llama3", request_timeout=360.0),
                         nodes=val_nodes,
                         output_path="val_dataset.json",
                     )
-        
+        return train_dataset, val_dataset
+    
+    def train_RAG(self) -> object:
+        train_dataset, val_dataset = self.generate_dataset()
+
         finetune_engine = SentenceTransformersFinetuneEngine(
             train_dataset,
             model_id="BAAI/bge-small-en",
@@ -89,7 +98,7 @@ class RAG_LLM:
         return embed_model
 
     
-    def evaluate_RAG(dataset, embed_model='local:BAAI/bge-small-en', top_k=5,verbose=False):
+    def evaluate_RAG(self, dataset, embed_model='local:BAAI/bge-small-en', top_k=5,verbose=False):
         '''a simple hit rate metric for evaluation, retrieve top-k documents with the query,
           and it's a hit if the results contain the relevant doc.'''
         corpus = dataset.corpus
@@ -103,7 +112,7 @@ class RAG_LLM:
         retriever = index.as_retriever(similarity_top_k=top_k)
 
         eval_results = []
-        for query_id, query in tqdm(queries.items()):
+        for query_id, query in queries.items():
             retrieved_nodes = retriever.retrieve(query)
             retrieved_ids = [node.node.node_id for node in retrieved_nodes]
             expected_id = relevant_docs[query_id][0]
@@ -127,4 +136,7 @@ class RAG_LLM:
 rag = RAG_LLM()
 text_prompt="summarise this file"
 rag.ask_RAG(text_prompt)
-# rag.evaluate_RAG()
+#Run to get corpus data
+# train, val = rag.generate_dataset()
+val_dataset = EmbeddingQAFinetuneDataset.from_json("/home/xavier002/Private_LLM/data/10k/val_dataset.json")
+rag.evaluate_RAG(val_dataset)
