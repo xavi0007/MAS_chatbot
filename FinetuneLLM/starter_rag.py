b6850757010cb5f2fa6fe096fe36950734cd2e2a
xavi0007 <XAVIER002@e.ntu.edu.sg>
2024-11-05 12:45:51 +0800

    RAG finetuning

--- FinetuneLLM/starter_rag.py
+++ FinetuneLLM/starter_rag.py
@@ -1,23 +1,130 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.schema import MetadataMode,TextNode
+from llama_index.finetuning import generate_qa_embedding_pairs,SentenceTransformersFinetuneEngine
+from llama_index.core.evaluation import EmbeddingQAFinetuneDataset
+from llama_index.llms.gemini import Gemini
+from llama_index.core import VectorStoreIndex
+from tqdm.notebook import tqdm
+import pandas as pd
+import os
+from typing import List
+
+class RAG_LLM:
+    def __init__(self) -> None:
+        # bge-base embedding model
+        Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+        # ollama
+        Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+        self.GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+        self.TRAIN_FILES = ["~/Private_LLM/data/10k/lyft_2021.pdf"]
+        self.VAL_FILES = ["~/Private_LLM/data/10k/uber_2021.pdf"]
+        self.INPUT_FILE = ["~/Private_LLM/data/paul_graham_essay.txt"]
+
+        self.TRAIN_CORPUS_FPATH = "~/Private_LLM/data/train_corpus.json"
+        self.VAL_CORPUS_FPATH = "~/Private_LLM/data/val_corpus.json"
+        
+        #preload
+        self.RAG_index = self.read_file(self.TRAIN_FILES)
+        
+
+    def load_corpus(self, files, verbose=False):
+        if verbose:
+            print(f"Loading files {files}")
+
+        reader = SimpleDirectoryReader(input_files=files)
+        docs = reader.load_data()
+        if verbose:
+            print(f"Loaded {len(docs)} docs")
+
+        parser = SentenceSplitter()
+        nodes = parser.get_nodes_from_documents(docs, show_progress=verbose)
+
+        if verbose:
+            print(f"Parsed {len(nodes)} nodes")
+
+        return nodes
+
+    def read_file(self, files: List[str]):
+        reader = SimpleDirectoryReader(input_files=files)
+        docs = reader.load_data()
+        print(f"Loaded {len(docs)} docs")
+        index = VectorStoreIndex.from_documents(
+        docs,)
+        return index
 
 
-reader = SimpleDirectoryReader(
-    input_files=["~/Private_LLM/data/paul_graham_essay.txt"]
-)
-docs = reader.load_data()
-print(f"Loaded {len(docs)} docs")
-# bge-base embedding model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+    def ask_RAG(self, query:str):
+        query_engine = self.RAG_index.as_query_engine()
+        response = query_engine.query(query)
+        print(response)
 
-# ollama
-Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+    def train_RAG(self) -> object:
+        train_nodes = self.load_corpus(self.TRAIN_FILES, verbose=True)
+        val_nodes = self.load_corpus(self.VAL_FILES, verbose=True)
 
-index = VectorStoreIndex.from_documents(
-    docs,
-)
+        train_dataset = generate_qa_embedding_pairs(
+                        llm=Ollama(model="llama3", request_timeout=360.0),
+                        nodes=train_nodes,
+                        output_path="train_dataset.json",
+                    )
+        val_dataset = generate_qa_embedding_pairs(
+                        llm=Gemini(model_name="gemini-1.5-flash", api_key=self.GOOGLE_API_KEY),
+                        nodes=val_nodes,
+                        output_path="val_dataset.json",
+                    )
+        
+        finetune_engine = SentenceTransformersFinetuneEngine(
+            train_dataset,
+            model_id="BAAI/bge-small-en",
+            model_output_path="test_model",
+            val_dataset=val_dataset,
+        )
 
-query_engine = index.as_query_engine()
-response = query_engine.query("What did the author do growing up?")
-print(response)
+        finetune_engine.finetune()
+        embed_model = finetune_engine.get_finetuned_model()
+        return embed_model
+
+    
+    def evaluate_RAG(dataset, embed_model='local:BAAI/bge-small-en', top_k=5,verbose=False):
+        '''a simple hit rate metric for evaluation, retrieve top-k documents with the query,
+          and it's a hit if the results contain the relevant doc.'''
+        corpus = dataset.corpus
+        queries = dataset.queries
+        relevant_docs = dataset.relevant_docs
+
+        nodes = [TextNode(id_=id_, text=text) for id_, text in corpus.items()]
+        index = VectorStoreIndex(
+            nodes, embed_model=embed_model, show_progress=True
+        )
+        retriever = index.as_retriever(similarity_top_k=top_k)
+
+        eval_results = []
+        for query_id, query in tqdm(queries.items()):
+            retrieved_nodes = retriever.retrieve(query)
+            retrieved_ids = [node.node.node_id for node in retrieved_nodes]
+            expected_id = relevant_docs[query_id][0]
+            is_hit = expected_id in retrieved_ids  # assume 1 relevant doc
+
+            eval_result = {
+                "is_hit": is_hit,
+                "retrieved": retrieved_ids,
+                "expected": expected_id,
+                "query": query_id,
+            }
+            eval_results.append(eval_result)
+        
+        df_bge = pd.DataFrame(eval_results)
+        hit_rate = df_bge["is_hit"].mean()
+        print(hit_rate)
+        return eval_results
+
+
+
+rag = RAG_LLM()
+text_prompt="summarise this file"
+rag.ask_RAG(text_prompt)
+# rag.evaluate_RAG()
